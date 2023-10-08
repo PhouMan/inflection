f457b5ee58fd0f36d35cd67ff5650d9ec8fdad63
Oscar Lam <oscarl9173665@gmail.com>
2023-10-08 15:01:23 -0400

    new

--- src/level.gd
+++ src/level.gd
@@ -12,3 +12,7 @@ func _on_back_pressed():
 
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://src/level_2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://src/level_3.tscn")
